4e2561a55a093e1d8e09abaca7922256ac4809de
fouad mogy <88947305+fouadmogy10@users.noreply.github.com>
2022-07-31 23:21:28 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,63 @@
+
+window.onscroll=function(){
+    if(scrollY>700){
+        let nav=document.getElementById("nav");
+        nav.classList.add("scroll");
+        nav.style.boxShadow="3px 4px 20px 6px #00000029"
+    }
+    else{
+        nav.classList.remove("scroll");
+    }
+    
+}
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml8 .circle-white',
+    scale: [0, 3],
+    opacity: [1, 0],
+    easing: "easeInOutExpo",
+    rotateZ: 360,
+    duration: 1100
+  }).add({
+    targets: '.ml8 .circle-container',
+    scale: [0, 1],
+    duration: 1100,
+    easing: "easeInOutExpo",
+    offset: '-=1000'
+  }).add({
+    targets: '.ml8 .circle-dark',
+    scale: [0, 1],
+    duration: 1100,
+    easing: "easeOutExpo",
+    offset: '-=600'
+  }).add({
+    targets: '.ml8 .letters-left',
+    scale: [0, 1],
+    duration: 1200,
+    offset: '-=550'
+  }).add({
+    targets: '.ml8 .bang',
+    scale: [0, 1],
+    rotateZ: [45, 15],
+    duration: 1200,
+    offset: '-=1000'
+  }).add({
+    targets: '.ml8',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1400
+  });
+
+anime({
+  targets: '.ml8 .circle-dark-dashed',
+  rotateZ: 360,
+  duration: 8000,
+  easing: "linear",
+  loop: true
+});
+$("document").ready(function () {
+    $(".loader").delay(3000).fadeOut();
+    
+});
